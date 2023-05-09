9c50766c2ddbc2155bb27c2420b11f5dad468946
omwami <ianalpha@protonmail.com>
2023-05-09 21:25:00 +0300

    create instance using **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,21 @@ import uuid
 
 class BaseModel():
     """ BaseModel class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ init vars """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                val = value
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    form = "%Y-%m-%dT%H:%M:%S.%f"
+                    val = datetime.datetime.strptime(value, form)
+                setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ define __str__ """
